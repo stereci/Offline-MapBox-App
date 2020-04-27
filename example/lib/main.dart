f7fccc0b368639f410d22dd0a94e936d5704cdf7
stereci <stereci@gmail.com>
2020-04-27 16:15:22 +0300

    Offline MapBox App First Commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,63 +1,139 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:mapbox_gl_example/full_map.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-import 'animate_camera.dart';
-import 'full_map.dart';
-import 'line.dart';
-import 'map_ui.dart';
-import 'move_camera.dart';
-import 'page.dart';
-import 'place_circle.dart';
-import 'place_symbol.dart';
-import 'scrolling_map.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path/path.dart';
 
-final List<ExamplePage> _allPages = <ExamplePage>[
-  MapUiPage(),
-  FullMapPage(),
-  AnimateCameraPage(),
-  MoveCameraPage(),
-  PlaceSymbolPage(),
-  LinePage(),
-  PlaceCirclePage(),
-  ScrollingMapPage(),
-];
+void main() => runApp(MyApp());
 
-class MapsDemo extends StatelessWidget {
-  void _pushPage(BuildContext context, ExamplePage page) async {
-    final location = Location();
-    final hasPermissions = await location.hasPermission();
-    if (hasPermissions != PermissionStatus.GRANTED) {
-      await location.requestPermission();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MapWidget());
+  }
+}
+
+class MapWidget extends StatefulWidget {
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
+
+class _MapWidgetState extends State<MapWidget> {
+  final CameraPosition _kInitialPosition;
+  final CameraTargetBounds _cameraTargetBounds;
+  static double defaultZoom = 12.0;
+
+  var _tilesLoaded = false;
+
+  CameraPosition _position;
+  MapboxMapController mapController;
+  bool _isMoving = false;
+  bool _compassEnabled = true;
+  MinMaxZoomPreference _minMaxZoomPreference =
+  const MinMaxZoomPreference(12.0, 18.0);
+  String _styleString = "mapbox://styles/mapbox/streets-v11";
+  bool _rotateGesturesEnabled = true;
+  bool _scrollGesturesEnabled = true;
+  bool _tiltGesturesEnabled = false;
+  bool _zoomGesturesEnabled = true;
+  bool _myLocationEnabled = false;
+  MyLocationTrackingMode _myLocationTrackingMode = MyLocationTrackingMode.None;
+
+  _MapWidgetState._(
+      this._kInitialPosition, this._position, this._cameraTargetBounds);
+
+  static CameraPosition _getCameraPosition() {
+    final latLng = LatLng(41.0082, 28.9784);
+    return CameraPosition(
+      target: latLng,
+      zoom: defaultZoom,
+    );
+  }
+  @override
+  initState() {
+    super.initState();
+    _copyTilesIntoPlace();
+  }  _copyTilesIntoPlace() async {
+    try {
+      await installOfflineMapTiles(join("assets", "cache.db"));
+    } catch (err) {
+      print(err);
+    }    setState(() {
+      this._tilesLoaded = true;
+    });
+  }
+
+  factory _MapWidgetState() {
+    CameraPosition cameraPosition = _getCameraPosition();
+
+    final cityBounds = LatLngBounds(
+      southwest: LatLng(40.9682, 29.0384),
+      northeast: LatLng(41.0582, 28.9184),
+    );
+
+    return _MapWidgetState._(
+        cameraPosition, cameraPosition, CameraTargetBounds(cityBounds));
+  }
+
+  void _onMapChanged() {
+    setState(() {
+      _extractMapInfo();
+    });
+  }
+
+  @override
+  void dispose() {
+    if (mapController != null) {
+      mapController.removeListener(_onMapChanged);
     }
+    super.dispose();
+  }
 
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (_) => Scaffold(
-              appBar: AppBar(title: Text(page.title)),
-              body: page,
-            )));
+  void _extractMapInfo() {
+    _position = mapController.cameraPosition;
+    _isMoving = mapController.isCameraMoving;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('MapboxMaps examples')),
-      body: ListView.builder(
-        itemCount: _allPages.length,
-        itemBuilder: (_, int index) => ListTile(
-          leading: _allPages[index].leading,
-          title: Text(_allPages[index].title),
-          onTap: () => _pushPage(context, _allPages[index]),
-        ),
-      ),
-    );
+    if (this._tilesLoaded) {
+      return Container(
+        child: _buildMapBox(context),
+      );
+    } else {
+      return Center(
+        child: new CircularProgressIndicator(),
+      );
+    }
   }
-}
 
-void main() {
-  runApp(MaterialApp(home: MapsDemo()));
+  MapboxMap _buildMapBox(BuildContext context) {
+    return MapboxMap(
+        onMapCreated: onMapCreated,
+        initialCameraPosition: this._kInitialPosition,
+        trackCameraPosition: true,
+        compassEnabled: _compassEnabled,
+        cameraTargetBounds: _cameraTargetBounds,
+        minMaxZoomPreference: _minMaxZoomPreference,
+        styleString: _styleString,
+        rotateGesturesEnabled: _rotateGesturesEnabled,
+        scrollGesturesEnabled: _scrollGesturesEnabled,
+        tiltGesturesEnabled: _tiltGesturesEnabled,
+        zoomGesturesEnabled: _zoomGesturesEnabled,
+        myLocationEnabled: _myLocationEnabled,
+        myLocationTrackingMode: _myLocationTrackingMode,
+        onCameraTrackingDismissed: () {
+          this.setState(() {
+            _myLocationTrackingMode = MyLocationTrackingMode.None;
+          });
+        });
+  }
+
+  void onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+    mapController.addListener(_onMapChanged);
+    _extractMapInfo();
+    setState(() {});
+  }
 }
